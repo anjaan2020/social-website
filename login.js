1c11572f776e5e346e839c1dc3afa7b043a8a543
anjaan2020 <76585104+anjaan2020@users.noreply.github.com>
2021-02-28 09:23:14 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,21 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCfizEOMbwcbZFtCjJI0YezTlL46dx_8-g",
+  authDomain: "social-website-a2c56.firebaseapp.com",
+  databaseURL: "https://social-website-a2c56-default-rtdb.firebaseio.com",
+  projectId: "social-website-a2c56",
+  storageBucket: "social-website-a2c56.appspot.com",
+  messagingSenderId: "577988341445",
+  appId: "1:577988341445:web:3d9e77c0054e2068c4caac",
+  measurementId: "G-HNH8X6N3Q6"
+};
+firebase.initializeApp(firebaseConfig);
+
+function addUser(){
+  user_name = document.getElementById("user_name").value; 
+  firebase.database().ref("/").child(user_name).update({
+    id : (user_name)
+  });
+localStorage.setItem("user_name",user_name);
+
+    window.location = "main_page.html";
+}
