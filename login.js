e9038382d0f91e249f5f220be1e2517fecc92cbd
anjaan2020 <76585104+anjaan2020@users.noreply.github.com>
2021-03-16 20:05:26 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,3 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyCfizEOMbwcbZFtCjJI0YezTlL46dx_8-g",
-  authDomain: "social-website-a2c56.firebaseapp.com",
-  databaseURL: "https://social-website-a2c56-default-rtdb.firebaseio.com",
-  projectId: "social-website-a2c56",
-  storageBucket: "social-website-a2c56.appspot.com",
-  messagingSenderId: "577988341445",
-  appId: "1:577988341445:web:3d9e77c0054e2068c4caac",
-  measurementId: "G-HNH8X6N3Q6"
-};
-firebase.initializeApp(firebaseConfig);
-
 function addUser(){
 localStorage.setItem("user_name",user_name);
 
