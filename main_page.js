f90b69b17dba96c6924ea77ec13a63e48a213472
anjaan2020 <76585104+anjaan2020@users.noreply.github.com>
2021-03-16 20:08:32 +0530

    Update main_page.js

--- main_page.js
+++ main_page.js
@@ -19,11 +19,7 @@ document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
 function addRoom()
 {
   room_name = document.getElementById("room_name").value;
-
-  firebase.database().ref("/").child(room_name).update({
-    purpose : "adding room name"
-  });
-
+  
     localStorage.setItem("room_name", room_name);
     
     window.location = "room.html";
