7a4179f36bb086b2aa369e9c98fcc02c8cded159
anjaan2020 <76585104+anjaan2020@users.noreply.github.com>
2021-03-16 20:11:27 +0530

    Update main_page.js

--- main_page.js
+++ main_page.js
@@ -27,7 +27,6 @@ function addRoom()
 
 function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
        Room_names = childKey;
-       console.log("Room Name - " + Room_names);
       row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
       document.getElementById("output").innerHTML += row;
     });
@@ -39,7 +38,6 @@ getData();
 
 function redirectToRoomName(name)
 {
-  console.log(name);
   localStorage.setItem("room_name", name);
     window.location = "room.html";
 }
